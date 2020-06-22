b16e63405c6451c4b0ddbee3c2e2adb25ae621ac
abc <abc@example.com>
2020-06-22 19:14:26 +0530

    22-6

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller {
 							'password' => $request->password,
 					]
 			]);
+
 			return $response->getBody();
 	    } catch (BadResponseException $e){
 			return response()->json('cant login', $e->getCode());
